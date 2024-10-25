f04fe2ad4f352b6db79d8e674a01636093e8eacc
0xGeN02 <manuelmateodgl02@gmail.com>
2024-10-25 10:54:37 +0200

    delete theme toggler

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,15 +1,12 @@
 // src/main.tsx
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './components/ThemeContext';
 import './styles/index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   );
 }
